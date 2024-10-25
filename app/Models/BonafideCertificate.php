3648c5d519de874c0c218000fb25feaf855e1699
manishnehwal21 <manishnehwal21@gmail.com>
2024-10-25 17:31:16 +0530

    Updated APIS

---
+++ app/Models/BonafideCertificate.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class BonafideCertificate extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table ='bonafide_certificate';
+    protected $fillable = [
+        'sr_no','stud_name','father_name','class_division','dob','dob_words','purpose','stud_id','issue_date_bonafide','academic_yr','IsGenerated','IsDeleted','IsIssued','generated_by'
+    ];
+}
