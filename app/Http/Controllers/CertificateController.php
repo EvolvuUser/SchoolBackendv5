92718cdb68503c24008dd09b5d52839b95d466dc
manishnehwal21 <manishnehwal21@gmail.com>
2024-11-05 11:42:17 +0530

    PercentageCertificate Code

--- app/Http/Controllers/CertificateController.php
+++ app/Http/Controllers/CertificateController.php
@@ -15,6 +15,8 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 use App\Models\SimpleBonafide;
 use App\Models\CasteBonafide;
 use App\Models\CharacterCertificate;
+use App\Models\PercentageCertificate;
+use App\Models\PercentageMarksCertificate;
 
 class CertificateController extends Controller
 {
@@ -817,5 +819,203 @@ class CertificateController extends Controller
                 return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
              }
     }
+    public function getSrnopercentagebonafide($id){
+        try{
+            $srnopercentagebonafide = DB::table('percentage_certificate')->orderBy('sr_no', 'desc')->first();
+            $studentinformation = DB::table('student')
+            ->join('parent', 'student.parent_id', '=', 'parent.parent_id')
+            ->join('section', 'section.section_id', '=', 'student.section_id')
+            ->join('class', 'class.class_id', '=', 'student.class_id')
+            ->where('student_id',$id)
+            ->select('class.class_id','class.name as classname', 'section.section_id','section.name as sectionname', 'parent.*', 'student.*') // Adjust select as needed
+            ->first();
 
+            if(is_null($studentinformation)){
+                return response()->json([
+                    'status'=> 200,
+                    'message'=>'Student information is not there',
+                    'data' =>$studentinformation,
+                    'success'=>true
+                 ]);
+              }
+            if (is_null($srnopercentagebonafide)) {
+                $data['sr_no'] = '1';
+                $data['date']  = Carbon::today()->format('Y-m-d');
+                $data['studentinformation']=$studentinformation;
+                if($studentinformation->classname == "10"){
+                   $class10subjects = DB::table('class10_subject_master')->get();
+                   $data['class10subjects'] = $class10subjects;
+                   $count = count($class10subjects);
+                   $data['subjectCount'] = $count;
+                }
+                else{
+                    $result = DB::table('subjects_higher_secondary_studentwise AS shs')
+                    ->join('subject_group AS grp', 'shs.sub_group_id', '=', 'grp.sub_group_id')
+                    ->join('subject_group_details AS grpd', 'grp.sub_group_id', '=', 'grpd.sub_group_id')
+                    ->join('subject_master AS shsm', 'grpd.sm_hsc_id', '=', 'shsm.sm_id')
+                    ->join('subject_master AS shs_op', 'shs.opt_subject_id', '=', 'shs_op.sm_id')
+                    ->join('stream', 'grp.stream_id', '=', 'stream.stream_id')
+                    ->select('shs.*', 'grp.sub_group_name', 'grpd.sm_hsc_id','shsm.name as subject_name', 'shsm.subject_type','shs_op.name as optional_sub_name','stream.stream_name')
+                    ->where('shs.student_id', $id)
+                    ->get();
+                    $data['classsubject'] = $result;
+                    $count = count($result);
+                    $data['subjectCount'] = $count;
+                    
+                }
+            }
+            else{
+                $data['sr_no'] = $srnopercentagebonafide->sr_no + 1 ;
+                $data['date']  = Carbon::today()->format('Y-m-d');
+                $data['studentinformation']=$studentinformation;
+                if($studentinformation->classname == "10"){
+                    $class10subjects = DB::table('class10_subject_master')->get();
+                    $data['class10subjects'] = $class10subjects;
+                    $count = count($class10subjects);
+                    $data['subjectCount'] = $count;
+                 }
+                 else{
+                    $result = DB::table('subjects_higher_secondary_studentwise AS shs')
+                    ->join('subject_group AS grp', 'shs.sub_group_id', '=', 'grp.sub_group_id')
+                    ->join('subject_group_details AS grpd', 'grp.sub_group_id', '=', 'grpd.sub_group_id')
+                    ->join('subject_master AS shsm', 'grpd.sm_hsc_id', '=', 'shsm.sm_id')
+                    ->join('subject_master AS shs_op', 'shs.opt_subject_id', '=', 'shs_op.sm_id')
+                    ->join('stream', 'grp.stream_id', '=', 'stream.stream_id')
+                    ->select('shs.*', 'grp.sub_group_name', 'grpd.sm_hsc_id', 'shsm.name as subject_name', 'shsm.subject_type', 'stream.stream_name', 'shs_op.name as optional_sub_name')
+                    ->where('shs.student_id', $id)
+                    ->get();
+                   $data['classsubject'] = $result;
+                   $count = count($result);
+                   $data['subjectCount'] = $count;
+                 }
+            }
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Bonafide Percentage Certificate SrNo.',
+                'data' =>$data,
+                'success'=>true
+             ]);
+        }
+        catch (Exception $e) {
+            \Log::error($e); // Log the exception
+            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+         }
+    }
+    public function downloadpercentagePDF(Request $request){
+        try{
+            $user = $this->authenticateUser();
+            $customClaims = JWTAuth::getPayload()->get('academic_yr');
+            
+            
+            $percentageCertificate = PercentageCertificate::create([
+            'roll_no' => $request->roll_no,
+            'stud_name' => $request->stud_name,
+            'class_division' => $request->class_division,
+            'percentage' => $request->percentage,
+            'stud_id' => $request->stud_id,
+            'certi_issue_date' => $request->date,
+            'academic_yr'=>$customClaims,
+            'IsGenerated'=> 'Y',
+            'IsDeleted'  => 'N',
+            'IsIssued'   => 'N',
+            'generated_by'=>Auth::user()->id,
+            ]);
+            
+            
+            $marksData = [];
+            foreach ($request->class as $mark) {
+                $marksData[] = [
+                    'sr_no' => $percentageCertificate->sr_no,
+                    'c_sm_id' => $mark['c_sm_id'],
+                    'marks' => $mark['marks'],
+                ];
+            }
+
+            PercentageMarksCertificate::insert($marksData);
+            return response()->json('data saved successfully');
+            }
+            catch (Exception $e) {
+                \Log::error($e); // Log the exception
+                return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+             }
+    }
+
+    public function percentagebonafideCertificateList(Request $request){
+        $searchTerm = $request->query('q');
+        try{
+        $user = $this->authenticateUser();
+        $customClaims = JWTAuth::getPayload()->get('academic_yr');
+        
+        $results = PercentageCertificate::where('class_division', 'LIKE', "%{$searchTerm}%")
+                                       ->where('academic_yr','LIKE',"%{$customClaims}%")
+                                       ->get();
+        
+        if($results->isEmpty()){
+            return response()->json([
+            'status'=> 200,
+            'message'=>'No Student Found for this Class',
+            'data' =>$results,
+            'success'=>true
+            ]);
+        }
+        else{
+        return response()->json([
+            'status'=> 200,
+            'message'=>'Student found for this Class are-',
+            'data' => $results,
+            'success'=>true
+            ]);
+          }
+        }
+        catch (Exception $e) {
+            \Log::error($e); // Log the exception
+            return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+         }
+    }
+    
+    public function updatepercentageisIssued(Request $request,$sr_no){
+        try{
+            $bondafidecertificateinfo = PercentageCertificate::find($sr_no);
+            $bondafidecertificateinfo->isGenerated = 'N';
+            $bondafidecertificateinfo->isIssued    = 'Y';
+            $bondafidecertificateinfo->isDeleted   = 'N';
+            $bondafidecertificateinfo->issued_date = Carbon::today()->format('Y-m-d');
+            $bondafidecertificateinfo->issued_by   = Auth::user()->id;
+            $bondafidecertificateinfo->update();
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Percentage Certificate Issued Successfully',
+                'data' => $bondafidecertificateinfo,
+                'success'=>true
+                ]);
+    
+            }
+            catch (Exception $e) {
+                \Log::error($e); // Log the exception
+                return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+            }
+    }
+
+    public function deletepercentageisDeleted(Request $request,$sr_no){
+        try{
+            $bondafidecertificateinfo = PercentageCertificate::find($sr_no);
+            $bondafidecertificateinfo->isGenerated = 'N';
+            $bondafidecertificateinfo->isIssued    = 'N';
+            $bondafidecertificateinfo->isDeleted   = 'Y';
+            $bondafidecertificateinfo->deleted_date = Carbon::today()->format('Y-m-d');
+            $bondafidecertificateinfo->	deleted_by   = Auth::user()->id;
+            $bondafidecertificateinfo->update();
+            return response()->json([
+                'status'=> 200,
+                'message'=>'Percentage Certificate Deleted Successfully',
+                'data' => $bondafidecertificateinfo,
+                'success'=>true
+                ]);
+    
+            }
+            catch (Exception $e) {
+                \Log::error($e); // Log the exception
+                return response()->json(['error' => 'An error occurred: ' . $e->getMessage()], 500);
+             }
+    }
 }
