dd0062c8e610dcb29c36c3fe497c4dc860f64432
manishnehwal21 <manishnehwal21@gmail.com>
2024-11-05 11:42:17 +0530

    PercentageCertificate Code

---
+++ app/Models/PercentageMarksCertificate.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class PercentageMarksCertificate extends Model
+{
+    use HasFactory;
+    public $timestamps = false;
+    protected $table ='percentage_marks_certificate';
+    protected $fillable = [
+        'sr_no','c_sm_id','marks'
+    ];
+}
