e4168815b96c6b9226ee23d3559e40f73cbf91d8
manishnehwal21 <manishnehwal21@gmail.com>
2025-04-22 15:01:19 +0530

    Update config externalapisfile

---
+++ config/externalapis.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'validate_staff_user' => 'https://aceventura.in/demo/evolvuUserService/validate_staff_user',
+    'get_school_details' => 'https://aceventura.in/demo/evolvuUserService/get_school_details',
+];
+
+?>
