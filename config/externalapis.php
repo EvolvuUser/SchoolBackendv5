ba8f9d5ea719191529ff1546f91876426d15de65
manishnehwal21 <manishnehwal21@gmail.com>
2025-04-24 10:52:40 +0530

    Updated code for teacher image and class teacher of for teacher

--- config/externalapis.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'validate_staff_user' => 'https://aceventura.in/demo/evolvuUserService/validate_staff_user',
-    'get_school_details' => 'https://aceventura.in/demo/evolvuUserService/get_school_details',
-];
-
-?>
