a2a251946aeb28f662ce9e19b654f2e60b797ad9
thelazybusycoder <devenasan@gmail.com>
2025-12-16 09:31:16 +0530

    update

--- app/Http/Controllers/AssessmentController.php
+++ app/Http/Controllers/AssessmentController.php
@@ -9287,9 +9287,8 @@ class AssessmentController extends Controller
         if ($request->login_type == 'T') {
             $str_array    =    $request->input('str_array');
             $str_classes =  str_replace(array('[', ']', '"'), '', $str_array);
-            $str_classes_array = explode(",", $str_classes);
+            $str_classes_array = explode("^", $str_classes);
 
-            return response()->json($request->all());
             if ($request->input('subject_id') == '') {
                 $data['subject_id'] = 0;
             } else {
