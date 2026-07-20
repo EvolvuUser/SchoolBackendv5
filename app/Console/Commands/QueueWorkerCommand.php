4a03ffa2239fb718612b7501c3fdfc755d39b8a7
manishnehwal21 <manishnehwal21@gmail.com>
2026-07-20 15:43:30 +0530

    added the changes

--- app/Console/Commands/QueueWorkerCommand.php
+++ app/Console/Commands/QueueWorkerCommand.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
 
 class QueueWorkerCommand extends Command
 {
