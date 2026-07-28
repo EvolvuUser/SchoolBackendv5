3678c21627e7198c688f99e5a7e6ea22d34bc57c
manishnehwal21 <manishnehwal21@gmail.com>
2026-07-28 12:43:39 +0530

    added UKG report card

--- app/Jobs/GenerateReportCardJob.php
+++ app/Jobs/GenerateReportCardJob.php
@@ -9,6 +9,7 @@ use App\Services\ReportCard\Class1To2BulkReportCardDataBuilder;
 use App\Services\ReportCard\Class9To10BulkReportCardDataBuilder;
 use App\Services\ReportCard\LkgBulkReportCardDataBuilder;
 use App\Services\ReportCard\NurseryBulkReportCardDataBuilder;
+use App\Services\ReportCard\UkgBulkReportCardDataBuilder;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -71,12 +72,13 @@ class GenerateReportCardJob implements ShouldQueue
                 break;
 
             case 'UKG':
+                $reportCardData = app(UkgBulkReportCardDataBuilder::class)
+                    ->build($this->class_id, $this->section_id, $this->academic_yr);
+
                 Pdf::loadView(
                     'reportcard.SACS.ukg_report_card_pdf_all',
                     [
-                        'student_id' => $this->student_id,
-                        'class_id' => $this->class_id,
-                        'academic_yr' => $this->academic_yr
+                        'reportCardData' => $reportCardData,
                     ]
                 )->save($fullPath);
                 break;
